456ec8972a5fa0910093f8cac88af7779bb85842
yakup878 <69221688+yakup878@users.noreply.github.com>
2021-01-23 01:41:48 +0300

    16 Ocak Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> aylar = new MyList<string>();
+            aylar.Add("Temmuz");
+
+            List<string> aylar2 = new List<string>();
+            Console.WriteLine(aylar2.Count);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //void -> emir kipi git yap bitir <- (ekle,sil,güncelle vs.)
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Eklendi.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Güncellendi.");
+        }
+        //geriye döndür
+        public int Topla(int sayi1, int sayi2  /*, params int[] numbers*/)
+        {
+            return sayi1 + sayi2;
+        } 
+        public void Topla2(int sayi1, int sayi2 )
+        {
+            Console.WriteLine(sayi1+sayi2);
+        }
+    }
+}
