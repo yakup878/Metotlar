0cde8130543f6c39c4999606652292463b1ac159
yakup878 <69221688+yakup878@users.noreply.github.com>
2021-01-26 02:38:37 +0300

    23 Ocak Ders Sonu

---
+++ ObjectOrientedProgramming2/CustomerManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgramming2
+{
+    //İş Sınıfları - Business Classes
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+
+        }
+    }
+}
---
+++ ObjectOrientedProgramming2/TuzelMusteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgramming2
+{
+    //Coorporate->Tuzel
+    class TuzelMusteri : Musteri //TuzelMusteri:Musteri -> miras - inheratance
+    {
+        public string VergiNo { get; set; }
+        public string SirketAdi { get; set; }
+    }
+}
